e77e25be60f4599fdd388043c68d214729927a7f
Joey Poon <joey@joeypoon.com>
2016-03-10 00:51:15 -0600

    subscribe even if mailer fails. validate uniqueness of email

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,10 +1,11 @@
 class Subscriber < ActiveRecord::Base
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   after_create :send_email
 
   private
 
     def send_email
       SubscriberMailer.welcome_email(self).deliver_now
+      true
     end
 end
